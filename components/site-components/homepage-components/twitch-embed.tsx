e336274959063e815d2fdc63260b4109a81a76f7
Coog-Esports <coogesports@gmail.com>
2026-09-16 19:25:54 -0400

    fix: update environment variable for Twitch embed component

--- components/site-components/homepage-components/twitch-embed.tsx
+++ components/site-components/homepage-components/twitch-embed.tsx
@@ -1,5 +1,5 @@
 export default function TwitchEmbed() {
-    const parent = process.env.NEXT_PUBLIC_TWITCH_PARENT ?? "localhost"
+    const parent = process.env.TWITCH_PARENT ?? "localhost"
   return (
     <section className="h-full w-full rounded-xl border border-red-500/30 bg-gray-950/90 p-4 text-white backdrop-blur-lg">
         <h2 className="mb-4 text-xl font-bold">
